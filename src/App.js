c20f88e029ac33af00c6bc54ee396b808aad12e1
lqmnhat1310 <119806917+lqmnhat1310@users.noreply.github.com>
2024-12-18 12:02:52 +0700

    #17. State - Kiểm Soát Data Ứng Dụng React

--- src/App.js
+++ src/App.js
@@ -3,31 +3,40 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 
+import React from "react";
+
 import MyComponent from "./components/MyComponent";
 
-const App = () => {
-    const count = useSelector((state) => state.counter.count);
-    const dispatch = useDispatch();
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World Le Quang Minh Nhat
+                <MyComponent></MyComponent>
+                {Math.random()}
+            </div>
+        );
+    }
+}
 
-    return (
-        <div>
-            Hello World Le Quang Minh Nhat
-            <MyComponent></MyComponent>
-        </div>
-        // <div className="App">
-        //     <header className="App-header">
-        //         <img src={logo} className="App-logo" alt="logo" />
-        //         <p>Hello World Le Quang Minh Nhat</p>
-        //         <div>Count = {count}</div>
-        //         <button onClick={() => dispatch(increaseCounter())}>
-        //             Increase
-        //         </button>
-        //         <button onClick={() => dispatch(decreaseCounter())}>
-        //             Decrease
-        //         </button>
-        //     </header>
-        // </div>
-    );
-};
+// const App = () => {
+//     const count = useSelector((state) => state.counter.count);
+//     const dispatch = useDispatch();
 
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo" />
+//                 <p>Hello World Le Quang Minh Nhat</p>
+//                 <div>Count = {count}</div>
+//                 <button onClick={() => dispatch(increaseCounter())}>
+//                     Increase
+//                 </button>
+//                 <button onClick={() => dispatch(decreaseCounter())}>
+//                     Decrease
+//                 </button>
+//             </header>
+//         </div>
+//     );
+// };
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,9 +3,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: "CyBot",
+        address: "Dong Nai",
+        age: 21,
+    };
     //JSX
     render() {
-        return <div>My first component</div>;
+        return (
+            <div>
+                My name is {this.state.name} and I'm from {this.state.address}
+            </div>
+        );
     }
 }
 
