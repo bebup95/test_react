43e3b45a7939a3662cf2d3aa420b97969bfb11ee
lqmnhat1310 <119806917+lqmnhat1310@users.noreply.github.com>
2024-12-19 15:43:45 +0700

    #25 . Conditional Output - Sử Dụng Câu Điều Kiện

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,19 +1,46 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+    state = {
+        isShowHide: true,
+    };
+
+    handleHideShow = () => {
+        return this.setState({
+            isShowHide: !this.state.isShowHide,
+        });
+    };
+
     render() {
         const { ListUsers } = this.props;
         return (
             <div>
-                {ListUsers.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div> My name's {user.name}</div>
-                            <div> My age's {user.age}</div>
-                            <hr />
-                        </div>
-                    );
-                })}
+                <button
+                    onClick={() => {
+                        this.handleHideShow();
+                    }}
+                >
+                    Click Me To Hide List Users Detail
+                </button>
+
+                {this.state.isShowHide && (
+                    <div>
+                        {ListUsers.map((user) => {
+                            return (
+                                <div
+                                    key={user.id}
+                                    className={
+                                        user.age % 2 === 0 ? "green" : "red"
+                                    }
+                                >
+                                    <div> My name's {user.name}</div>
+                                    <div> My age's {user.age}</div>
+                                    <hr />
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
             </div>
         );
     }
