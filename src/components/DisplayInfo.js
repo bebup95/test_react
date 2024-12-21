465a628408e5e5fc35eda9ac99103e1cb6069c25
lqmnhat1310 <nhat0908464570@gmail.com>
2024-12-21 14:39:35 +0700

    #28 Sử Dụng Image Sao Cho Hiệu Quả

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DisplayInfo.scss";
+import logo from "./../logo.svg";
 
 class DisplayInfo extends React.Component {
   state = {
@@ -16,6 +17,7 @@ class DisplayInfo extends React.Component {
     const { ListUsers } = this.props;
     return (
       <div className="display-infor-container">
+        <img src={logo} />
         <button
           onClick={() => {
             this.handleHideShow();
