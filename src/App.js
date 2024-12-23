da06633dbb5e6f5f0d031afc9003b9784f4d41fc
lqmnhat1310 <119806917+lqmnhat1310@users.noreply.github.com>
2024-12-23 15:31:22 +0700

    #46. Setup Bootstrap 5 & React Router Dom v6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "./App.scss";
-import React from "react";
-import MyComponent from "./components/MyComponent";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        Hello World Le Quang Minh Nhat
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div className="app-container">Hello World</div>;
+};
 
 export default App;
