d6fe063c9695aae823cb8ad1674edf78bb4ec342
lqmnhat1310 <119806917+lqmnhat1310@users.noreply.github.com>
2024-12-24 21:03:10 +0700

    #51. Index Routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,10 @@ const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Project 1</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">Project 1</Navbar.Brand> */}
+        <NavLink to="/" className="navbar-brand">
+          Project 1
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -28,9 +31,9 @@ const Header = () => {
           </Nav>
           <Nav>
             <NavDropdown title="Setting" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Log In</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Log Out</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+              <NavDropdown.Item>Log In</NavDropdown.Item>
+              <NavDropdown.Item>Log Out</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>My home page component</div>;
+};
+
+export default HomePage;
