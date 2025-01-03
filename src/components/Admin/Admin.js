23a2e564fae4746aaf3e87b8d20f508cf09548b7
lqmnhat1310 <119806917+lqmnhat1310@users.noreply.github.com>
2025-01-03 14:52:44 +0700

    #70. API Login | Đăng nhập

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,7 +3,6 @@ import "./Admin.scss";
 import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Admin = (props) => {
@@ -26,17 +25,6 @@ const Admin = (props) => {
           <Outlet />
         </div>
       </div>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
     </div>
   );
 };
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,12 +1,29 @@
 import { useState } from "react";
 import "../Auth/Login.scss";
+import { postLogin } from "../../services/apiServices";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    alert("me");
+  const handleLogin = async (props) => {
+    //validate
+
+    //submit api
+    let data = await postLogin(email, password);
+    console.log("check data:", data);
+
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
+      navigate("/");
+    }
+
+    if (data && +data.EC !== 0) {
+      toast.error(data.EM);
+    }
   };
   return (
     <section className="vh-100 gradient-custom">
@@ -15,7 +32,7 @@ const Login = (props) => {
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
             <div className="card login-card">
               <div className="card-body p-5 text-center">
-                <div className="mb-md-5 mt-md-4 pb-5">
+                <div className=" mt-md-4 pb-5">
                   <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
                   <p className="text-muted mb-5">
                     Please enter your login and password!
@@ -75,6 +92,16 @@ const Login = (props) => {
                       Sign Up
                     </a>
                   </p>
+                </div>
+
+                <div className="mt-2 return-homepage">
+                  <span
+                    onClick={() => {
+                      navigate("/");
+                    }}
+                  >
+                    &#60;&#60; Back To Homepage
+                  </span>
                 </div>
               </div>
             </div>
